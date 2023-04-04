fbb1aa8f8c1ce6f068d4575883f7b6d65b8b8b68
jojohenry <jshenry91@yahoo.com>
2023-04-03 23:20:32 -0500

    adjustments made

--- variables.tf
+++ variables.tf
@@ -1,32 +1,52 @@
 #/root 
 
 #vpc
-variable "tag_name" {}
+variable "tag_name" {
+  default = "Week22"
+}
 variable "vpc_id" {
   type        = string
   description = "This configures the vpc cidr"
+  default    = "10.0.0.0/16"
 }
 variable "vpc_cidr_block" {
   type        = string
   description = "This configures the vpc cidr"
+  default     = "10.0.0.0/16"
 }
 
 #ec2
-variable "instance_type" {}
-variable "keyname" {}
-variable "ami" {}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "keyname" {
+  default = "JojoKeys"
+}
+variable "ami" {
+  default = "ami-0dfcb1ef8550277af"
+}
 
 #rds
-variable "engine" {}
-variable "engine_version" {}
-variable "db_name" {}
+variable "engine" {
+  default = "mysql"
+}
+variable "engine_version" {
+  default = "8.0.28"
+}
+variable "db_name" {
+  default = "rdsdb"
+}
 variable "username" {
   sensitive = true
+  default  = "user"
 }
 variable "password" {
   sensitive = true
+  default = "password"
 }
 
-variable "region" {}
+variable "region" {
+  default = "us-east-1"
+}
 
 
